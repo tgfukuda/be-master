52af4dbafd3012168e3f6f617696535689177e88
tgfukuda <luktiger793@gmail.com>
2023-05-30 13:54:01 +0900

    gateway http logger

--- main.go
+++ main.go
@@ -132,7 +132,8 @@ func runGatewayServer(config util.Config, store db.Store) {
 	}
 
 	log.Info().Msgf("start http gateway server at %s", listener.Addr().String())
-	err = http.Serve(listener, mux)
+	handler := gapi.HttpLogger(mux)
+	err = http.Serve(listener, handler)
 	if err != nil {
 		log.Fatal().Err(err).Msg("cannot start http gateway server")
 	}
